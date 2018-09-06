beae9cf23618611372bdacb9a0d8726d8303bc1e
shoji_naoto <onyxnaoto@gmail.com>
2018-09-06 21:22:38 +0900

    edit-0906
    リュージョンを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = '0906upload-test'
